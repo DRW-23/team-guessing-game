66d4008b22e2a4168a1a976730012786e1e74d6a
ckasunara <>
2025-05-03 08:14:59 +0530

    first chaos

--- guessing_game.py
+++ guessing_game.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random 
 number = random.randint(1, 100) 
 print("Guess a number between 1 and 10") 
@@ -7,11 +6,10 @@ if guess == number:
 print("You win!") 
 else: 
 print(f"Wrong! The number was {number}") 
-=======
 import random
 
 while True:
-    number = random.randint(1, 10)
+    number = random.randint(1, 50)
     print("Guess a number between 1 and 10")
     guess = int(input())
 
@@ -23,4 +21,3 @@ while True:
     print("Play again? (y/n)")
     if input().lower() != 'y':
         break
->>>>>>> 44a13ad96717ddcd60911b6ae85eaec387a469b3
